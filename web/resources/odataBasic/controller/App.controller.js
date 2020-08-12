3a04b055b3cb067870d907ecfcd7e57511c0d092
aelweb <axeleberhard.leistner@freenet.de>
2020-08-12 11:01:21 +0200

    Exercise 5.6

--- web/resources/odataBasic/controller/App.controller.js
+++ web/resources/odataBasic/controller/App.controller.js
@@ -20,9 +20,25 @@ sap.ui.define([
 			var oTable = this.getView().byId("bpTable");
 
 			//To-Do Bind Model to table
-			oTable.setModel(bpModel);
-			oTable.setEntitySet("BusinessPartners");
-			oTable.setInitiallyVisibleFields("PARTNERID,COMPANYNAME,PARTNERROLE");
+			function fnLoadMetadata() {
+				try {
+					oTable.setModel(bpModel);
+					oTable.setEntitySet("BusinessPartners");
+					var oMeta = bpModel.getServiceMetadata();
+					var headerFields = "";
+					for (var i = 0; i < oMeta.dataServices.schema[0].entityType[0].property.length; i++) {
+						var property = oMeta.dataServices.schema[0].entityType[0].property[i];
+						headerFields += property.name + ",";
+					}
+					oTable.setInitiallyVisibleFields(headerFields);
+				} catch (e) {
+					console.log(e.toString());
+				}
+			}
+			bpModel.attachMetadataLoaded(bpModel, function() {
+				fnLoadMetadata();
+			});
+			fnLoadMetadata();
 		},
 
 		onErrorCall: function(oError) {
